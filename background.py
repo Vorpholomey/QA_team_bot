cf5619449d9697e32816514479532703aff0fa62
@rzhulin <rzhulin@rtk-element.ru>
2023-11-25 15:53:23 +0700

    add background process

---
+++ background.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from threading import Thread
+
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return "Я жив еще"
+def run():
+    app.run(host='0.0.0.0', port = 80)
+
+def keep_alive():
+    t=Thread(target=run)
+    t.start()
